a8edb3480be1b8758b5761f8c115dcd1dc428dce
Christoffer Jonsson <schnavzer@protonmail.com>
2025-08-07 11:47:38 +0200

    new version

--- dropdown.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-document.addEventListener('DOMContentLoaded', function () {
-    let menuIcon = document.querySelector('.menu-icon');
-    let dropdownList = document.querySelector('.dropdown-list');
-
-    menuIcon.addEventListener('click', function () {
-        dropdownList.classList.toggle('visible');
-    });
-});
